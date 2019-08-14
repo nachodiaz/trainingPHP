2d7fe41ce8e86824c771c90d964397892fb21a15
Ignacio Díaz <ignaciosergiodiaz@gmail.com>
2019-08-14 00:26:07 -0400

    my first Hello World in PHP training PHP + GIT

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+    echo 'Hola Mundo PHP';
+    print '<p>Otra forma de imprimir en vez de echo es print, aun que todo tiene que estar dentro, no se puede separar por comas </p>';
+
+    
+?>
